5435f9d3e0dfeef368faaed1873db07dfa00b442
Sri <sri@rts-macbook-air.home>
2015-03-31 11:59:08 -0400

    first commit

--- MyTableViewController.h
+++ MyTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MyTableViewController.h
 //  Table View
 //
 //  Created by Sri on 3/31/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MyTableViewController : UITableViewController
 
 @end
-
